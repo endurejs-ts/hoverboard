c339930daa87325e985177efcffedb7a90a39357
resforxofficial <paedeogim@gmail.com>
2024-10-15 00:31:25 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,103 @@
 import * as https from 'https';
 import { IncomingMessage, ServerResponse } from 'http';
+import { parse } from 'url';
+import * as fs from 'fs';
+import * as path from 'path';
 import cors from 'cors';
 import qs from 'qs';
 import bp from 'body-parser';
-import { parse } from 'url';
 
 class HoverBoard {
-    static onboard() {}
+    // onboard() 함수 - HoverTheServer 인스턴스를 반환
+    static onboard() {
+        return new HoverTheServer();
+    }
 }
 
+type MiddlewareFaster = (req: IncomingMessage, res: ServerResponse, next: () => void) => void;
 type RideRequest = {};
-type RideResult = {};
+
+type RideResultOptions = "file" | "code";
+type RideResult = {
+    html: (filePath: string, options: { type: RideResultOptions }) => void;
+};
 
 interface RideCallBackBody {
     req: RideRequest;
-    res: RideResult
+    res: RideResult;
 }
 
 class HoverTheServer {
     private routes: { [key: string]: (body: RideCallBackBody) => void } = {};
+    private middlewares: (MiddlewareFaster)[] = [];
 
+    // 미들웨어 등록
+    faster(middleware: MiddlewareFaster) {
+        this.middlewares.push(middleware);
+    }
+
+    // fast() 미들웨어 예시
+    fast() {
+        return (req: IncomingMessage, res: ServerResponse, next: () => void) => {
+            console.log("Fast middleware executed");
+            next(); // 다음 미들웨어로 넘어감
+        };
+    }
+
+    // 경로 설정
     ride(pathname: string, callback: (body: RideCallBackBody) => void) {
         this.routes[pathname] = callback;
     }
 
+    // fly 메서드 - 서버 실행
     fly(port: number, callback: () => void) {
         const server = https.createServer((req: IncomingMessage, res: ServerResponse) => {
-            const parsedUrl = parse(req.url || "", true);
-            const routeHandler = this.routes[parsedUrl.pathname || ""];
+            const parsedUrl = parse(req.url || '', true);
+            const routeHandler = this.routes[parsedUrl.pathname || ''];
 
-            if (routeHandler) {
-                const requsetBody: RideRequest = {};
-                const responseBody: RideResult = {};
+            let i = 0;
+            const next = () => {
+                const middleware = this.middlewares[i++];
+                if (middleware) {
+                    middleware(req, res, next);
+                } else if (routeHandler) {
+                    const requestBody: RideRequest = {};
+                    const responseBody: RideResult = {
+                        html: (pathORcode: string, options: { type: RideResultOptions }) => {
+                            if (options.type === 'file') {
+                                try {
+                                    const fileContent = fs.readFileSync(pathORcode, 'utf-8');
+                                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                                    res.end(fileContent);
+                                } catch (err) {
+                                    res.writeHead(500, { 'Content-Type': 'text/plain' });
+                                    res.end(err);
+                                }
+                            }
 
-                routeHandler({
-                    req: requsetBody,
-                    res: responseBody,
-                });
+                            else if (options.type === "code") {
+                                try {
+                                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                                    res.end(pathORcode);
+                                } catch (e) {
+                                    res.writeHead(500, { 'Content-Type': 'text/plain' });
+                                    res.end(e);
+                                }
+                            }
+                        },
+                    };
 
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify(responseBody));
-            }
-
-            else {
-                res.writeHead(404, { 'Content-Type': 'text/plain' });
-                res.end("Not Found");
-            }
+                    // 라우트 핸들러 실행
+                    routeHandler({
+                        req: requestBody,
+                        res: responseBody,
+                    });
+                } else {
+                    res.writeHead(404, { 'Content-Type': 'text/plain' });
+                    res.end('Not Found');
+                }
+            };
+            next(); // 첫 번째 미들웨어 실행
         });
 
         server.listen(port, callback);
